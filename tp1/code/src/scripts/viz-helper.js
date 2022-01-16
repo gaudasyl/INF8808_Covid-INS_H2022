67ac1caec1117543d0c3f069657b1dfd39d29404
williamGlazer <willcavanagh@hotmail.com>
2022-01-16 11:54:23 -0500

    added final TP1

--- tp1/code/src/scripts/viz-helper.js
+++ tp1/code/src/scripts/viz-helper.js
@@ -5,6 +5,13 @@ export function updateHeader () {
   // TODO: Select the existing header element and append to it :
   //        * An 'h1' element with text : 'TP1'
   //        * A'div' element with text : 'Bienvenue au cours INF8808 : Visualisation de données.'
+  var header = d3.select('header')
+  header
+    .append('h1')
+    .text('TP1')
+  header
+    .append('div')
+    .text('Bienvenue au cours INF8808 : Visualisation de données.')
 }
 
 /**
@@ -28,17 +35,27 @@ export function updateHeader () {
  *
  *   @returns {object[]} The generated data
  */
+
 export function generateData () {
   // TODO: Generate the data structure described above and return it.
-  return []
+  const size = d3.randomUniform(1, 11)()
+  const dataGenerator = d3.randomUniform(1, 100)
+
+  const data = Array.from({length: size}, () => {
+    return {
+      x: Math.floor(dataGenerator()),
+      y: Math.floor(dataGenerator())
+    }
+  })
+
+  return data
 }
 
 /**
  * @returns {number} The current number of circles displayed in the scatter plot.
  */
 export function getDotCount () {
-  // TODO : Return number of currently displayed circles
-  return 0
+  return d3.selectAll('.dot').nodes().length
 }
 
 /**
@@ -49,6 +66,11 @@ export function updateInfoPanel () {
   // TODO: Get the current dot count and diplay it in the information panel.
   // Make sure the label says 'point' or 'points' depending how many points there are.
   // see : getDotCount()
+  var nDots = getDotCount()
+  d3.select('.dot-count')
+    .text(nDots)
+  d3.select('.dot-label')
+    .text(nDots > 1 ? 'points' : 'point')
 }
 
 /**
@@ -59,4 +81,7 @@ export function updateInfoPanel () {
  */
 export function styleCircles (g) {
   // TO DO: Select all the circles and set their fill and radius as specified above
+  d3.selectAll('.dot')
+    .attr('fill', '#07BEB8')
+    .attr('r', 5)
 }
