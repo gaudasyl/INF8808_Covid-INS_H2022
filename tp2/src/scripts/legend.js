242d87ae61aef342fe5b2041f2abb4795ca6716a
Matthieu <matthieu.beaud@hotmail.fr>
2022-01-18 14:58:48 -0500

    Legende

--- tp2/src/scripts/legend.js
+++ tp2/src/scripts/legend.js
@@ -8,4 +8,20 @@ export function draw (data, color) {
   // TODO : Generate the legend in the div with class "legend". Each SVG rectangle
   // should have a width and height set to 15.
   // Tip : Append one div per legend element using class "legend-element".
+  console.log(data)
+  console.log(color)
+  var legend = d3.select('.legend')
+  var legendElements = legend.selectAll('div')
+    .data(data)
+    .enter()
+    .append('div')
+    .attr('class', 'legend-element')
+  legendElements.append('svg').attr('width', 20).attr('height', 15)
+    .append('rect')
+    .attr('width', 15)
+    .attr('height', 15)
+    .attr('fill', element => color(element))
+    .attr('stroke-width', 1)
+    .attr('stroke', 'black')
+  legendElements.append('text').text(element => element)
 }
