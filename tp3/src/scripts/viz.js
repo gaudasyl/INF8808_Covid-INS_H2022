a8a36cfe6596d93bb8362a017a572a632cce3358
baptiste-pauletto1 <baptistep.contact@gmail.com>
2022-02-09 18:46:14 -0500

    update(viz.js) : Done.

--- tp3/src/scripts/viz.js
+++ tp3/src/scripts/viz.js
@@ -6,7 +6,18 @@
  * @param {object[]} data The data to be displayed
  */
 export function setColorScaleDomain (colorScale, data) {
-  // TODO : Set domain of color scale
+  // Getting the max value possible in our domain.
+  let maxValueCount = d3.max(
+    data.map(
+      (element) => {
+        return element.Counts
+      }
+    )
+  )
+  
+  // Update our colorScale accordingly.
+  colorScale
+    .domain([0,maxValueCount])
 }
 
 /**
@@ -15,7 +26,13 @@ export function setColorScaleDomain (colorScale, data) {
  * @param {object[]} data The data to use for binding
  */
 export function appendRects (data) {
-  // TODO : Append SVG rect elements
+  // TODO : Creating our rects in the dedicated #graph-g frame.
+  d3.select('#graph-g')
+    .selectAll('rect')
+    .data(data)
+    .enter()
+    .append('g')
+    .append('rect')
 }
 
 /**
@@ -27,7 +44,26 @@ export function appendRects (data) {
  * @param {Function} range A utilitary funtion that could be useful to generate a list of numbers in a range
  */
 export function updateXScale (xScale, data, width, range) {
-  // TODO : Update X scale
+  // Getting the min and max values using d3 tools.
+  let minValueYear = d3.min(
+    data.map(
+      (element) => {
+        return element.Plantation_Year
+      }
+    )
+  )
+  let maxValueYear = d3.max(
+    data.map(
+      (element) => {
+        return element.Plantation_Year
+      }
+    )
+  )
+
+  // Updating the xScale accordingly.
+  xScale
+    .domain(range(minValueYear,maxValueYear))
+    .range([0,width])
 }
 
 /**
@@ -38,8 +74,17 @@ export function updateXScale (xScale, data, width, range) {
  * @param {number} height The height of the diagram
  */
 export function updateYScale (yScale, neighborhoodNames, height) {
-  // TODO : Update Y scale
-  // Make sure to sort the neighborhood names alphabetically
+  
+  // Sorting the neighborhood names alphabetically
+  let sortedNeighborhoodNames = neighborhoodNames.sort((a,b) => {
+    return d3.ascending(a,b)
+  })
+
+  // Updating the Y scale with the sorted neighborhoods.
+  yScale
+    .domain(sortedNeighborhoodNames)
+    .range([0, height])
+  
 }
 
 /**
@@ -48,7 +93,9 @@ export function updateYScale (yScale, neighborhoodNames, height) {
  *  @param {*} xScale The scale to use to draw the axis
  */
 export function drawXAxis (xScale) {
-  // TODO : Draw X axis
+  // Drawing the X axis using a top axis.
+  d3.select('.x.axis')
+    .call(d3.axisTop(xScale))
 }
 
 /**
@@ -58,14 +105,20 @@ export function drawXAxis (xScale) {
  * @param {number} width The width of the graphic
  */
 export function drawYAxis (yScale, width) {
-  // TODO : Draw Y axis
+  // Drawing Y axis using a right one and shifting it to the right of the diagram.
+  d3.select('.y.axis')
+    .call(d3.axisRight(yScale))
+    .attr('transform','translate(' + width + ',0)')
 }
 
 /**
  * Rotates the ticks on the X axis 45 degrees towards the left.
  */
 export function rotateXTicks () {
-  // TODO : Rotate X axis' ticks
+  // Rotating the ticks negatively in order to look like the demo.
+  d3.select(".x.axis")
+    .selectAll('text')
+      .attr("transform", "rotate(-45)");
 }
 
 /**
@@ -77,5 +130,13 @@ export function rotateXTicks () {
  * @param {*} colorScale The color scale used to set the rectangles' colors
  */
 export function updateRects (xScale, yScale, colorScale) {
-  // TODO : Set position, size and fill of rectangles according to bound data
+  // Updating rectangles according to the formerly defined scales.
+  d3.select('#graph-g')
+    .selectAll('rect')
+    .attr('x', (data) => xScale(data.Plantation_Year))
+    .attr('y', (data) => yScale(data.Arrond_Nom))
+    .attr('fill', (data) => colorScale(data.Counts))
+    .attr('width', xScale.bandwidth())
+    .attr('height', yScale.bandwidth())
+
 }
