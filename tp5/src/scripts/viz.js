3836e23311e1551755eada136a7e80f7e8e2237b
Matthieu <matthieu.beaud@hotmail.fr>
2022-03-14 14:32:55 -0400

    map and markers drawn, but can't show name on hover :'(

--- tp5/src/scripts/viz.js
+++ tp5/src/scripts/viz.js
@@ -6,6 +6,19 @@
  */
 export function colorDomain (color, data) {
   // Set the color domain
+  // Initiating a set to get all continents once
+  const siteTypes = new Set()
+  // Capture each continent correctly
+  data.features.forEach((feature) => {
+    siteTypes.add(feature.properties.TYPE_SITE_INTERVENTION)
+  })
+  // Tuning the set into an array and sorting it
+  const arraySiteTypes = Array.from(siteTypes)
+  arraySiteTypes.sort(d3.ascending)
+  // Setting the color scale
+  color = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(arraySiteTypes)
+  return color
 }
 
 /**
@@ -17,6 +30,19 @@ export function colorDomain (color, data) {
  */
 export function mapBackground (data, path, showMapLabel) {
   // TODO : Generate the map background and set the hover handlers
+  d3.select('#map-g').selectAll('g')
+    .data(data.features)
+    .enter()
+    .append('g')
+    .append('path')
+    .attr('d', path)
+    .attr('class', 'area')
+    .attr('stroke-width', 1)
+    .attr('stroke', 'white')
+    .attr('fill', 'lightgrey')
+    .on('mouseover', function (element) {
+      showMapLabel(element, path)
+    })
 }
 
 /**
@@ -30,6 +56,14 @@ export function mapBackground (data, path, showMapLabel) {
 export function showMapLabel (d, path) {
   // TODO : Show the map label at the center of the neighborhood
   // by calculating the centroid for its polygon
+  console.log(d)
+  const label = d.properties.NOM
+  const pos = path.centroid(d.geometry)
+  d3.select(d)
+    .append('text')
+    .text(label)
+    .attr('x', pos[0])
+    .attr('y', pos[1])
 }
 
 /**
@@ -44,4 +78,21 @@ export function mapMarkers (data, color, panel) {
   // Their color corresponds to the type of site and their outline is white.
   // Their radius is 5 and goes up to 6 while hovered by the cursor.
   // When clicked, the panel is displayed.
+  d3.select('#marker-g')
+    .selectAll('circle')
+    .data(data.features)
+    .enter()
+    .append('circle')
+    .classed('marker', true)
+    .attr('r', 5)
+    .attr('fill', (feature) => color(feature.properties.TYPE_SITE_INTERVENTION))
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1)
+    .on('mouseover', function (d) {
+      d3.select(this).attr('r', 6)
+    })
+    .on('mouseout', function (d) {
+      d3.select(this).attr('r', 5)
+    })
+    .on('mouseclick', panel.show)
 }
