cf4cce4915be9c50005a583a61ec0d3310432d36
Matthieu <matthieu.beaud@hotmail.fr>
2022-03-14 14:53:32 -0400

    legende done, mais pas ordonnée meme si créée à partir d'1 scale ordonné

--- tp5/src/scripts/legend.js
+++ tp5/src/scripts/legend.js
@@ -9,4 +9,16 @@ import d3Legend from 'd3-svg-legend'
 export function drawLegend (colorScale, g) {
   // TODO : Generate the legend
   // For help, see : https://d3-legend.susielu.com/
+  g.append('g')
+    .attr('class', 'legendOrdinal')
+    .attr('transform', 'translate(50,120)')
+    .attr('font-family', 'Open Sans Condensed')
+
+  var legendOrdinal = d3Legend.legendColor()
+    .shape('path', d3.symbol().type(d3.symbolCircle).size(300)())
+    .scale(colorScale)
+    .title('Légende')
+
+  g.select('.legendOrdinal')
+    .call(legendOrdinal)
 }
