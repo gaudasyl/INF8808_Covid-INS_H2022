6f2ad16f6ecbcebda2812e949360c4f3c3bf3960
Matthieu <matthieu.beaud@hotmail.fr>
2022-03-22 08:47:21 -0400

    Empty Code Project

---
+++ projet/src/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import d3Tip from 'd3-tip'
+
+/**
+ * @file This file is the entry-point for the the code for the Project for the course INF8808.
+ * @author Matthieu Beaud ...
+ * @version v1.0.0
+ */
+
+(function (d3) {
+  
+})(d3)
