5e68470bb8397c25ebbe771ce98ab9e769da5c36
williamGlazer <willcavanagh@hotmail.com>
2022-04-01 16:22:53 -0400

    bugfix

--- projet/src/index.js
+++ projet/src/index.js
@@ -12,17 +12,17 @@ import * as viz from './scripts/viz.js'
 
 
 // start and end dates to define
-const startDate = new Date(2020, 0, 1)
-const endDate = new Date(2022, 0, 1)
+const startDate = new Date(2020, 0, 1);
+const endDate = new Date(2022, 0, 1);
 
-    (function (d3) {
-        d3.csv('./moving_avg_dataset.csv').then(function (data) {
-            console.log(data)
-            viz.DrawCount(data, startDate, endDate)
-            viz.DrawSmallMultiple(data, startDate, endDate)
-        })
+(function (d3) {
+    d3.csv('./moving_avg_dataset.csv').then(function (data) {
+        console.log(data)
+        viz.DrawCount(data, startDate, endDate)
+        viz.DrawSmallMultiple(data, startDate, endDate)
+    })
 
-        d3.csv('./merged_covid_dataset.csv').then((data) => {
-            viz.DrawCovidViz(data, startDate, endDate)
-        })
-    })(d3)
+    d3.csv('./merged_covid_dataset.csv').then((data) => {
+        viz.DrawCovidViz(data, startDate, endDate)
+    })
+})(d3)
