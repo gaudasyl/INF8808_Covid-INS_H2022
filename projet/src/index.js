dd255af7e1089ab3ffc7eced4aa2049203b8d2a5
williamGlazer <willcavanagh@hotmail.com>
2022-04-01 16:18:10 -0400

    added covid dataset

--- projet/src/index.js
+++ projet/src/index.js
@@ -10,14 +10,19 @@ import * as viz from './scripts/viz.js'
  * @version v1.0.0
  */
 
-(function (d3) {
-    d3.csv('./moving_avg_dataset.csv').then(function (data) {
-        console.log(data)
-        // start and end dates to define
-        const startDate = new Date(2020, 0, 1)
-        const endDate = new Date(2022, 0, 1)
-        viz.DrawCount(data, startDate, endDate)
-        viz.DrawCovidViz()
-        viz.DrawSmallMultiple(data, startDate, endDate)
-    })
-})(d3)
+
+// start and end dates to define
+const startDate = new Date(2020, 0, 1)
+const endDate = new Date(2022, 0, 1)
+
+    (function (d3) {
+        d3.csv('./moving_avg_dataset.csv').then(function (data) {
+            console.log(data)
+            viz.DrawCount(data, startDate, endDate)
+            viz.DrawSmallMultiple(data, startDate, endDate)
+        })
+
+        d3.csv('./merged_covid_dataset.csv').then((data) => {
+            viz.DrawCovidViz(data, startDate, endDate)
+        })
+    })(d3)
